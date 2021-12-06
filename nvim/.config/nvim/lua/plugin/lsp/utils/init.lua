de762fed71ed722836ad69fcca61597f3ebd35c1
Matthijs Padding <mppadding@protonmail.com>
2021-12-06 11:14:04 +0100

    Cleaned up branch

--- nvim/.config/nvim/lua/plugin/lsp/utils/init.lua
+++
@@ -1,6 +0,0 @@
-local module = {
-    range = require("plugin.lsp.utils.range"),
-    text_edit = require("plugin.lsp.utils.text_edit"),
-}
-
-return module
--- nvim/.config/nvim/lua/refactor/init.lua
+++
@@ -1,3 +0,0 @@
-return {
-    func = require("refactor.function"),
-}
