2012325ba4e4cb12afdd28d384d39cf70a909912
Matthijs Padding <mppadding@protonmail.com>
2022-09-12 16:12:08 +0200

    [Icons] Added Icon for DapBreakpoint

--- nvim/.config/nvim/lua/lsp/icons.lua
+++ nvim/.config/nvim/lua/lsp/icons.lua
@@ -4,5 +4,6 @@ vim.fn.sign_define("DiagnosticSignError",   { text = "", texthl = "GruvboxRed
 vim.fn.sign_define("DiagnosticSignWarn",    { text = "", texthl = "GruvboxYellow" })
 vim.fn.sign_define("DiagnosticSignInfo",    { text = "", texthl = "GruvboxBlue" })
 vim.fn.sign_define("DiagnosticSignHint",    { text = "", texthl = "GruvboxAqua" })
+vim.fn.sign_define("DapBreakpoint",         { text = "🛑", texthl = "GruvboxRed"})
 
 --vim.api.nvim_set_hl(0, 'STS_highlight', colors.GruvboxOrangeBold)
